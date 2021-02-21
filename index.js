18147c716f5b6bcf2fe06107cbd74e6d5237b3b3
Smagic Jones <>
2021-02-21 16:42:55 +0000

    added sound on click

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const cells = document.querySelectorAll('.cell');
 
+const sequence = {
+	cell: true,
+	
+}
+
+
+
 for (let cell = 0; cell < cells.length; cell++) {
 	cells[cell].onclick = ( () => {
 		cells[cell].classList.toggle("on");
+		console.log(cells[cell].classList);
+		const sound = new Audio("sounds/crash.mp3");
+		sound.play();
 	})
 }
