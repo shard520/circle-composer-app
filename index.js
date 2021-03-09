377f56d05ba7876a61d8ee397115cd97f72fbb87
shard520 <65344196+shard520@users.noreply.github.com>
2021-03-09 22:55:18 +0100

    Add play/stop sequence functionality using the web audio API: add function to use an oscillator to play a note. Add function called playSequence to loop through cellsArray by using setTimeout to playNote if a cell is on, increment currentNote and then call itself recursively according to BPM value, once sequence is complete, reset currentNote. Add function to stop the sequence by clearing the timeout when the stop button is pressed.

--- index.js
+++ index.js
@@ -1,17 +1,106 @@
+'use strict';
+
+// Create new audio context oscillator
+const audioContext = new AudioContext();
+
+// Play button
+const playBtn = document.querySelector('.play__btn');
+// Stop button
+const stopBtn = document.querySelector('.stop__btn');
+
+// DOM circles arranged in a wheel of 16 beats
 const cells = document.querySelectorAll('.cell');
 
-const sequence = {
-	cell: true,
-	
+// Array of booleans used by loop to determine whether to sound each beat when playing
+const cellsArray = [
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+  false,
+];
+
+// Current note position in sequence
+let currentNote = 0;
+
+// Initial BPM value
+let bpmValue = 60;
+
+// BPM used by timer to play 16th notes according to current BPM
+let BPM = parseInt(((60 / bpmValue) * 1000) / 4);
+
+// Variable used to stop and start the timer
+let timer;
+
+function playNote() {
+  // Create oscillator
+  const oscillator = audioContext.createOscillator();
+  // Connect oscillator to audio output
+  oscillator.connect(audioContext.destination);
+  // Change frequency of oscillator note to middle C from the default 440Hz
+  oscillator.detune.value = 300;
+  // Play oscillator for 150ms
+  oscillator.start(audioContext.currentTime);
+  oscillator.stop(audioContext.currentTime + 0.15);
 }
 
+function playSequence() {
+  // Set timer based on current bpm to play a note if a cell is on
+  timer = setTimeout(function () {
+    if (cellsArray[currentNote]) {
+      playNote();
+    }
 
+    // Advance sequence to the next note
+    currentNote++;
 
+    // Reset currentNote at the end of the sequence
+    if (currentNote >= cellsArray.length) currentNote = 0;
+
+    // Call sequence recursively
+    playSequence();
+  }, BPM);
+}
+
+// Update cellsArray by checking if on class is present on current cell
+function storeCellValue(cell) {
+  cells[cell].classList.contains('on')
+    ? (cellsArray[cell] = true)
+    : (cellsArray[cell] = false);
+}
+
+// Play confirmation sound if cell has been changed to on
+function playConfirmationSound(cell) {
+  if (cells[cell].classList.contains('on')) {
+    playNote();
+  }
+}
+
+// Toggle a cell on or off, store the new value in cellsArray and call playConfirmationSound
+function toggleOnOff(cell) {
+  cells[cell].classList.toggle('on');
+  storeCellValue(cell);
+  playConfirmationSound(cell);
+}
+
+// Add event listener to each cell to toggle on or off when clicked
 for (let cell = 0; cell < cells.length; cell++) {
-	cells[cell].onclick = ( () => {
-		cells[cell].classList.toggle("on");
-		console.log(cells[cell].classList);
-		const sound = new Audio("sounds/crash.mp3");
-		sound.play();
-	})
+  cells[cell].addEventListener('click', () => toggleOnOff(cell));
 }
+
+// Add event listener to play button to call playSequence when clicked
+playBtn.addEventListener('click', playSequence);
+
+// Add event listener to stop button to clear timer when clicked
+stopBtn.addEventListener('click', () => clearTimeout(timer));
