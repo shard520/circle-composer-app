b0abaea3846ff3276e0b5c4f7e83b89ccd070a87
shard520 <65344196+shard520@users.noreply.github.com>
2021-03-28 23:27:57 +0200

    Add tempo and gain controls: Controls can be changed with a range slider or text input, updating one will update the other via oninput event listeners. Text inputs have error messages which are displayed when an invalid figure is inputted prompting the user to input between the allowed range. Tempo changes call updateTempo to update the BPM variable which is called recursively in the playSequence timer, as well as to control the note length in playNote. Gain controls update the gainNode which is attached to the oscillator.

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 'use strict';
 
-// Create new audio context
-const audioContext = new AudioContext();
-
 // Play button
 const playStopBtn = document.querySelector('.play-stop-btn');
-
 // DOM circles arranged in a wheel of 16 beats
 const cells = document.querySelectorAll('.cell');
+
+// Tempo range slider
+const tempoSlider = document.querySelector('#tempoSlider');
+// Tempo text input
+const tempoInput = document.querySelector('#tempoInput');
+// Tempo error message
+const tempoError = document.querySelector('#tempoErrorMsg');
+
+// Gain range slider
+const gainSlider = document.querySelector('#gainSlider');
+// Gain text input
+const gainInput = document.querySelector('#gainInput');
+// Gain error message
+const gainError = document.querySelector('#gainErrorMsg');
 
 // Array of booleans used by loop to determine whether to sound each beat when playing
 const cellsArray = [
@@ -29,14 +39,21 @@ const cellsArray = [
   false,
 ];
 
+// Create new audio context
+const audioContext = new AudioContext();
+// Gain node
+const gainNode = audioContext.createGain();
+// Initial gain
+gainNode.gain.value = 0.5;
+
 // Current note position in sequence
 let currentNote = 0;
 
 // Initial BPM value
-let bpmValue = 60;
+const initialBpmValue = 60;
 
 // BPM used by timer to play 16th notes according to current BPM
-let BPM = parseInt(((60 / bpmValue) * 1000) / 4);
+let BPM = parseInt(((60 / initialBpmValue) * 1000) / 4);
 
 // Variable used to stop and start the timer
 let timer;
@@ -44,13 +61,13 @@ let timer;
 function playNote() {
   // Create oscillator
   const oscillator = audioContext.createOscillator();
-  // Connect oscillator to audio output
-  oscillator.connect(audioContext.destination);
+  // Connect oscillator to gain node then audio output
+  oscillator.connect(gainNode).connect(audioContext.destination);
   // Change frequency of oscillator note to middle C from the default 440Hz
   oscillator.detune.value = 300;
-  // Play oscillator for 150ms
+  // Play oscillator for 90% of the length of current note (90% to avoid overlaps)
   oscillator.start(audioContext.currentTime);
-  oscillator.stop(audioContext.currentTime + 0.15);
+  oscillator.stop(audioContext.currentTime + (BPM / 1000) * 0.9);
 }
 
 // Check if sequence is currently playing then stop or start sequence accordingly and update button text and styles
@@ -108,6 +125,11 @@ function toggleOnOff(cell) {
   playConfirmationSound(cell);
 }
 
+function updateTempo(newTempo) {
+  // Update BPM variable used in playSequence timer and playNote note length
+  BPM = parseInt(((60 / newTempo) * 1000) / 4);
+}
+
 // Add event listener to each cell to toggle on or off when clicked
 for (let cell = 0; cell < cells.length; cell++) {
   cells[cell].addEventListener('click', () => toggleOnOff(cell));
@@ -115,3 +137,39 @@ for (let cell = 0; cell < cells.length; cell++) {
 
 // Add event listener to play/stop button to call playOrStop when clicked
 playStopBtn.addEventListener('click', playOrStop);
+
+// Detect tempo change on range slider and update current tempo
+tempoSlider.oninput = function (event) {
+  const input = event.target.value;
+  updateTempo(input);
+  tempoInput.value = input;
+};
+
+// Detect tempo change on tempo text input, check if it's a valid tempo and either display error message or update current tempo
+tempoInput.oninput = function (event) {
+  const input = Number(event.target.value.trim());
+
+  if (input >= 40 && input <= 240) {
+    tempoError.classList.add('u-hidden');
+    updateTempo(input);
+    tempoSlider.value = input;
+  } else tempoError.classList.remove('u-hidden');
+};
+
+// Detect gain change and update gainNode on oscillator
+gainSlider.oninput = function (event) {
+  const input = event.target.value / 100;
+  gainNode.gain.value = input;
+  gainInput.value = Math.round(input * 100);
+};
+
+// Detect gain change on gain text input, check if it's a valid gain and either display error message or update current gain
+gainInput.oninput = function (event) {
+  const input = Number(event.target.value.trim()) / 100;
+
+  if (input >= 0 && input <= 1) {
+    gainError.classList.add('u-hidden');
+    gainNode.gain.value = input;
+    gainSlider.value = input;
+  } else gainError.classList.remove('u-hidden');
+};
